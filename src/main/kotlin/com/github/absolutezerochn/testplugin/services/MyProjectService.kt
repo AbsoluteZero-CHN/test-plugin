e271455c387fbd33e5ca3bf63cca63250d331399
GitHub Action <action@github.com>
2023-09-14 08:02:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/absolutezerochn/testplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/absolutezerochn/testplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.absolutezerochn.testplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.absolutezerochn.testplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
